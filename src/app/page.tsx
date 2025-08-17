bb75ea641866556d820f213c167d00d564f3ba04
MAS40se7en <manir.saker@gmail.com>
2025-08-17 12:50:42 -0700

    03:Authentication Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,78 @@
+'use client';
+
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [error, setError] = useState("");
+
+  const { data: session, isPending } = authClient.useSession();
+
+  function onSubmit() {
+    authClient.signUp.email({
+      email,
+      name,
+      password
+    }, {
+      onError: (res) => {
+        console.log(res);
+        setError(res.error.message)
+      },
+      onSuccess: (res) => {
+        window.alert("User created successfully");
+      }
+    })
+  }
+  
+  function onLogin() {
+    authClient.signIn.email({
+      email,
+      password
+    }, {
+      onError: (res) => {
+        console.log(res);
+        setError(res.error.message)
+      },
+      onSuccess: (res) => {
+        window.alert("User created successfully");
+      }
+    })
+  }
+
+  if (isPending) {
+    return <div>Loading...</div>
+  }
+
+  if (session) {
+    return (
+      <div>
+        <h1>Welcome {session.user.name}</h1>
+        <Button onClick={() => authClient.signOut()}>Sign Out</Button>
+      </div>
+    )
+  }
+
   return (
-    <div className="text-3xl font-bold text-red-500">
-      HELLO WORLD
-      <Button variant={"secondary"}>Click me</Button>
+    <div>
+      <div>
+      <h1>Create User</h1>
+      <Input type="text" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
+      <Input type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Button onClick={onSubmit}>Create User</Button>
+      {error && <p className="text-red-500">{error}</p>}
+      </div>
+      <div>
+        <h1>Login</h1>
+        <Input type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <Button onClick={onLogin}>Login</Button>
+      </div>
     </div>
   );
 }
