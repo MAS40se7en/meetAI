c692751fb4f7178a0ddb6ed94b86bbbed147c272
MAS40se7en <manir.saker@gmail.com>
2025-08-17 11:46:38 -0700

    01:setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="text-3xl font-bold text-red-500">
       HELLO WORLD
+      <Button variant={"secondary"}>Click me</Button>
     </div>
   );
 }
