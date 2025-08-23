388df5b1823d3335315b0640124dda2ac2479ba5
MAS40se7en <manir.saker@gmail.com>
2025-08-23 18:52:28 +0200

    13:agents filters

--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -1,13 +1,25 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
-import { PlusIcon } from "lucide-react"
+import { PlusIcon, XIcon } from "lucide-react"
 import { NewAgentDialog } from "./new-agent-dialog"
 import { useState } from "react"
+import { useAgentsFilters } from "../../hooks/use-agents-filters"
+import { AgentsSearchFilter } from "./agents-search-filter"
+import { DEFAULT_PAGE } from "@/constants"
 
 export const AgentsListHeader = () => {
+    const [filters, setFilters] = useAgentsFilters();
     const [isDialogOpen, setIsDialogOpen] = useState(false)
 
+    const isAnyFilterModified = !!filters.search;
+
+    const onClearFilters = () => {
+        setFilters({
+            search: "",
+            page: DEFAULT_PAGE
+        })
+    }
 
     return (
         <>
@@ -19,6 +31,15 @@ export const AgentsListHeader = () => {
                     <PlusIcon />
                     Add Agent
                 </Button>
+            </div>
+            <div className="flex items-center gap-x-2 p-1">
+                <AgentsSearchFilter />
+                {isAnyFilterModified && (
+                    <Button variant="outline" size="sm" onClick={onClearFilters}>
+                        <XIcon />
+                        Cancel
+                    </Button>
+                )}
             </div>
         </div>
         </>
